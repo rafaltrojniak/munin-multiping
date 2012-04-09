57625382bbe6a82baa22e6e4bcfceb8666d10f01
Rafał Trójniak <rafal@trojniak.net>
2012-04-09 02:18:42 +0200

    Dokumentacja i segregacja

--- multiping.erl
+++ multiping.erl
@@ -1,35 +1,83 @@
 #!/usr/bin/escript
+
 main(["config"])->
 	showConfig();
 main([])->
-	getValues();
+	Pids=forkPings(getHosts()),
+	readResults(Pids).
 main(Unknown)->
 	io:format("Unknown parameters ~p",[Unknown]).
 
 
-getValues() ->
-	Pids=forkPings(getHosts()),
-	readResults(Pids).
 
+% Forks ping command for each host
 forkPings([]) ->
 	[];
 forkPings([Host|HTail]) ->
-	Command=lists:concat(["ping -nq ", getPingCommand(), " ", Host]),
-	io:format("~s\n",[Command]),
+	Command=lists:concat(["ping -nq ", getPingArgs(), " ", Host]),
 	Pid=open_port({spawn,Command},[]),
 	[{Host,Pid}|forkPings(HTail)].
 
+% Parses an reads results
 readResults([])->
 	ok;
-readResults([{Host,Pid}|Tail]) ->
+readResults(PidList) ->
 	receive
 		{Pid,{data,Data}} ->
-			RTT=getRTTFromResponse(Data),
-			Lost=getLostFromResponse(Data),
-			printSingleVal(Host,RTT,Lost),
-			readResults(Tail)
+			case lists:keyfind(Pid,2,PidList) of
+				false -> readResults(PidList);
+				{Host,Pid} ->
+					RTT=getRTTFromResponse(Data),
+					Lost=getLostFromResponse(Data),
+					printSingleVal(Host,RTT,Lost),
+					NewPidList=lists:keydelete(Pid,2,PidList),
+					readResults(NewPidList)
+			end
 	end.
 
+
+%% Munin protocol helpers
+
+% Prints configuration block
+showConfig()->
+	io:format("graph_title Multiping\n",[]),
+	Hosts=getHosts(),
+	lists:foreach(fun(Host)->printSingleConfig(Host) end,Hosts),
+	ok.
+
+% Prints configuration for single host
+printSingleConfig(Host) ->
+	EHost=escapeHost(Host),
+	io:format("~s_rtt.label ~s RTT\n",[EHost,Host]),
+	io:format("~s_lost.label ~s Lost packets\n",[EHost,Host]),
+	ok.
+
+% Prints vlues for single host
+printSingleVal(Host,RTT,Lost)->
+	EHost=escapeHost(Host),
+	io:format("~s_rtt.value ~s\n",[EHost,RTT]),
+	io:format("~s_lost.value  ~s\n",[EHost,Lost]),
+	ok.
+
+% Esape string to use as field name
+escapeHost(Host)->
+	re:replace(Host,"\\.","_",[global,{return,list}]).
+
+%% Ping command output scrapers
+
+% Fetches Round-Trip-Time
+getRTTFromResponse(Data)->
+	case re:run(Data,"min/avg/max.*\\s\\d+(?:\\.\\d+)?/(\\d+(?:\\.\\d+)?)/\\d+(?:\\.\\d+)?",[{capture,all_but_first,list}]) of
+		{match,[Val]} -> Val;
+		_ -> nomatch
+	end.
+
+% Fetches count of lost packets
+getLostFromResponse(Data)->
+	case re:run(Data,"received, (\\d+)% packet loss, time",[{capture,all_but_first,list}]) of
+		{match,[Val]} -> Val;
+		_ -> nomatch
+	end.
 getRTTFromResponse(Data)->
 	case re:run(Data,"min/avg/max.*\\s\\d+(?:\\.\\d+)?/(\\d+(?:\\.\\d+)?)/\\d+(?:\\.\\d+)?",[{capture,all_but_first,list}]) of
 		{match,[Val]} -> Val;
@@ -42,35 +90,19 @@ getLostFromResponse(Data)->
 		_ -> nomatch
 	end.
 
-showConfig()->
-	io:format("graph_title Multiping\n",[]),
-	Hosts=getHosts(),
-	lists:foreach(fun(Host)->printSingleConfig(Host) end,Hosts),
-	ok.
+%% ENV Readings
 
-printSingleConfig(Host) ->
-	EHost=escapeHost(Host),
-	io:format("~s_rtt.label ~s RTT\n",[EHost,Host]),
-	io:format("~s_lost.label ~s Lost packets\n",[EHost,Host]),
-	ok.
-
-printSingleVal(Host,RTT,Lost)->
-	EHost=escapeHost(Host),
-	io:format("~s_rtt.value ~s\n",[EHost,RTT]),
-	io:format("~s_lost.value  ~s\n",[EHost,Lost]),
-	ok.
-
+% Returns host list to ping
 getHosts()->
 	case os:getenv("host") of 
 		false -> [];
 		Hosts -> string:tokens(Hosts," \t")
 	end.
 
-getPingCommand()->
+% Returns ping command args
+getPingArgs()->
 	case os:getenv("ping_args") of 
 		false -> "-c 5 ";
 		Hosts -> Hosts
 	end.
 
-escapeHost(Host)->
-	re:replace(Host,"\\.","_",[global,{return,list}]).
